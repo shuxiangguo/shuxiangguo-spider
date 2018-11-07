a186823b03ab610e8e00e7804b828158aac0c33c
shuxiangguo <shuxiangguo_li@163.com>
2018-11-07 12:18:21 +0800

    直接获取拉钩网的职位信息

---
+++ lagou-spider.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+"""
+@author: shuxiangguo
+@file: project_demo.py
+@time: 2018-11-07 11:09:40
+"""
+
+
+import requests
+from lxml import etree
+import time
+
+def request_list_page():
+	url = "https://www.lagou.com/jobs/positionAjax.json?needAddtionalResult=false"
+
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+		'Referer': 'https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput=',
+		'Cookie': 'JSESSIONID=ABAAABAAAFCAAEG9493B8D684AA4F784118331596D6092A; _ga=GA1.2.2116619760.1540627605; user_trace_token=20181027160646-3fa7e102-d9bf-11e8-8232-5254005c3644; LGUID=20181027160646-3fa7e55a-d9bf-11e8-8232-5254005c3644; index_location_city=%E5%85%A8%E5%9B%BD; X_HTTP_TOKEN=5fa1c0fc3069a510b1f3b74d87bdf0f9; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22166b4c064565c8-0d315805c28162-9393265-921600-166b4c064571e9%22%2C%22%24device_id%22%3A%22166b4c064565c8-0d315805c28162-9393265-921600-166b4c064571e9%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24latest_referrer_host%22%3A%22%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%7D%7D; TG-TRACK-CODE=index_search; _gid=GA1.2.566468228.1541559981; PRE_UTM=; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1540627607,1540697568,1541560000; LGSID=20181107110642-2703e950-e23a-11e8-8eda-525400f775ce; PRE_HOST=cn.bing.com; PRE_SITE=https%3A%2F%2Fcn.bing.com%2F; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2F; _gat=1; LGRID=20181107111100-c0cc3de2-e23a-11e8-86f8-5254005c3644; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1541560258; SEARCH_ID=398db06e67ca4a2298c7372f07cee3f3'
+	}
+
+	data = {
+		'first': False,
+		'pn': 1,
+		'kd': 'python'
+	}
+
+	for x in  range(1, 30):
+		data['pn'] = x
+		response = requests.post(url, headers=headers, data=data)
+		print(response.json())
+
+		# 为了避免爬去太快被服务器屏蔽，可以用time模块sleep()一会
+		time.sleep(1)
+
+
+def main():
+	request_list_page()
+
+
+if __name__ == '__main__':
+    main()
